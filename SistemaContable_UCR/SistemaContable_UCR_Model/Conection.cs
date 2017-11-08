8d8058c4d1cb669a3ed166c11e7eb7c6ea4abe06
xxPEKESxx <ale.2.15@hotmail.com>
2017-11-07 23:13:13 -0600

    proyecto modelo agregado
    modelo con clase de conexion en blanco

---
+++ SistemaContable_UCR/SistemaContable_UCR_Model/Conection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaContable_UCR_Model
+{
+    public class Class1
+    {
+    }
+}
