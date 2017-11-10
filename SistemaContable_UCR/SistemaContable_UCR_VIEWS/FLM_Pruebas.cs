86f54ef7412c6982c5482f2b4d57fd809336602a
xxPEKESxx <ale.2.15@hotmail.com>
2017-11-10 13:57:17 -0600

    asdf
    sdfgh

---
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/FLM_Pruebas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace SistemaContable_UCR_VIEWS
+{
+    public partial class FLM_Pruebas : MetroForm
+    {
+        public FLM_Pruebas()
+        {
+            InitializeComponent();
+        }
+
+        private void FLM_Pruebas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
