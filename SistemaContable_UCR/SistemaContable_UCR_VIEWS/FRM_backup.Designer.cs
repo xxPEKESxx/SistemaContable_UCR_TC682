dad4a9f9cb8561f4babdb3f79efe0a50a004db47
xxPEKESxx <ale.2.15@hotmail.com>
2017-11-10 15:25:14 -0600

    dfghjk.
    ertyjk

--- SistemaContable_UCR/SistemaContable_UCR_VIEWS/FRM_backup.Designer.cs
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/FRM_backup.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FRM_backup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FRM_backup";
             this.Text = "FRM_backup";
+            this.Load += new System.EventHandler(this.FRM_backup_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- SistemaContable_UCR/SistemaContable_UCR_VIEWS/FRM_backup.cs
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/FRM_backup.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace SistemaContable_UCR_VIEWS
 {
-    public partial class FRM_backup : Form
+    public partial class FRM_backup : MetroForm
     {
         public FRM_backup()
         {
             InitializeComponent();
+        }
+
+        private void FRM_backup_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
