1ff4a9c0c1ab41669868ec4f0bf938f2d7847706
jafet chevez <jafet.chevez14@gmail.com>
2017-11-10 15:00:56 -0600

    ...

--- SistemaContable_UCR/SistemaContable_UCR_Views/Program.cs
+++ SistemaContable_UCR/SistemaContable_UCR_Views/Program.cs
@@ -13,7 +13,7 @@ namespace SistemaContable_UCR_VIEWS
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
