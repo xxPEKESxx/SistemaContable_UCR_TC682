3343236ca4705f5d4aca52eec6ef2b2719db47e3
Randal Vinicio Trejos Rodríguez <ranvitr@hotmail.com>
2017-11-10 23:17:55 -0600

    mjjiijji

---
+++ SistemaContable_UCR/Pruebas/GestorUsuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaContable_UCR_Busisness;
+namespace Pruebas
+{
+    [TestClass]
+    public class GestorUsuarios
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CoordinadorDeUsuarios coordinadorUsuarios = new CoordinadorDeUsuarios();
+            Console.Write(coordinadorUsuarios.login("1", "123"));
+        }
+    }
+}
