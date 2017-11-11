4a26d3421ddd129c832a1bd22f661bae1749f038
jafet chevez <jafet.chevez14@gmail.com>
2017-11-10 19:52:06 -0600

    creacion de pruebas

---
+++ SistemaContable_UCR/SistemaContable_UCR_Pruebas/PruebaGestorDeProductos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SistemaContable_UCR_Pruebas
+{
+    [TestClass]
+    public class PruebaGestorDeProductos
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
