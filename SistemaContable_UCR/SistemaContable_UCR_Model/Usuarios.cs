a8994d832daba1a98c3102005389f8e051dba058
Randal Vinicio Trejos Rodríguez <ranvitr@hotmail.com>
2017-11-10 22:22:05 -0600

    Model USuario

--- SistemaContable_UCR/SistemaContable_UCR_Model/Usuarios.cs
+++ SistemaContable_UCR/SistemaContable_UCR_Model/Usuarios.cs
@@ -8,5 +8,11 @@ namespace SistemaContable_UCR_Model
 {
     public class Usuarios
     {
+        public int ID { get; set; }
+        public string DNI { get; set; }
+        public string UserName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserPassword { get; set; }
+
     }
 }
