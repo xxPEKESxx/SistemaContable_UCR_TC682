e9811ad84d9d32476a681aa9ff0154a368b0cf52
jafet chevez <jafet.chevez14@gmail.com>
2017-11-10 21:12:31 -0600

    coordinador

--- SistemaContable_UCR/SistemaContable_UCR_Busisness/CoordinadorDeTransacciones.cs
+++ SistemaContable_UCR/SistemaContable_UCR_Busisness/CoordinadorDeTransacciones.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SistemaContable_UCR_Model;
+using SistemaContable_UCR_DataAccess;
+
 
 namespace SistemaContable_UCR_Busisness
 {
     public class CoordinadorDeTransacciones
     {
 
-        public void Agregar(SistemaContable_UCR_Model.Transacciones laNuevaTransaccion)
+        public void Save(SistemaContable_UCR_Model.Transacciones laNuevaTransaccion)
         {
+            GestorDeTransacciones gestor = new GestorDeTransacciones();
+
+            gestor.Save(laNuevaTransaccion);
         }
 
-        public void Editar(SistemaContable_UCR_Model.Transacciones laTransaccion)
+        public void Update(SistemaContable_UCR_Model.Transacciones laTransaccion)
         {
+            GestorDeTransacciones gestor = new GestorDeTransacciones();
 
+            gestor.Update(laTransaccion);
         }
 
         public SistemaContable_UCR_Model.Transacciones ObtenerTransaccionPorId(int id)
