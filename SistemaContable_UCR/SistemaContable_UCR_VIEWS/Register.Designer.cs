97f6f431d1e7274c99286052dee6ea7b71a3feb9
xxPEKESxx <ale.2.15@hotmail.com>
2017-11-10 19:35:51 -0600

    vistas de register
    y login

--- SistemaContable_UCR/SistemaContable_UCR_VIEWS/Register.Designer.cs
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/Register.Designer.cs
@@ -28,21 +28,279 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.registro_dni = new MetroFramework.Controls.MetroTextBox();
+            this.registro_nombre = new MetroFramework.Controls.MetroTextBox();
+            this.registro_apellidos = new MetroFramework.Controls.MetroTextBox();
+            this.register_password = new MetroFramework.Controls.MetroTextBox();
+            this.registro_passwordConfirn = new MetroFramework.Controls.MetroTextBox();
+            this.checkbox_vistaContrasela = new MetroFramework.Controls.MetroCheckBox();
+            this.icoUCR = new System.Windows.Forms.Label();
+            this.registro_Atras = new MetroFramework.Controls.MetroTile();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
+            // 
+            // registro_dni
+            // 
+            // 
+            // 
+            // 
+            this.registro_dni.CustomButton.Image = null;
+            this.registro_dni.CustomButton.Location = new System.Drawing.Point(288, 1);
+            this.registro_dni.CustomButton.Name = "";
+            this.registro_dni.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.registro_dni.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.registro_dni.CustomButton.TabIndex = 1;
+            this.registro_dni.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.registro_dni.CustomButton.UseSelectable = true;
+            this.registro_dni.CustomButton.Visible = false;
+            this.registro_dni.Lines = new string[0];
+            this.registro_dni.Location = new System.Drawing.Point(23, 80);
+            this.registro_dni.MaxLength = 32767;
+            this.registro_dni.Multiline = true;
+            this.registro_dni.Name = "registro_dni";
+            this.registro_dni.PasswordChar = '\0';
+            this.registro_dni.PromptText = "CEDULA";
+            this.registro_dni.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.registro_dni.SelectedText = "";
+            this.registro_dni.SelectionLength = 0;
+            this.registro_dni.SelectionStart = 0;
+            this.registro_dni.ShortcutsEnabled = true;
+            this.registro_dni.Size = new System.Drawing.Size(328, 41);
+            this.registro_dni.TabIndex = 0;
+            this.registro_dni.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.registro_dni.UseSelectable = true;
+            this.registro_dni.WaterMark = "CEDULA";
+            this.registro_dni.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.registro_dni.WaterMarkFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // registro_nombre
+            // 
+            // 
+            // 
+            // 
+            this.registro_nombre.CustomButton.Image = null;
+            this.registro_nombre.CustomButton.Location = new System.Drawing.Point(288, 1);
+            this.registro_nombre.CustomButton.Name = "";
+            this.registro_nombre.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.registro_nombre.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.registro_nombre.CustomButton.TabIndex = 1;
+            this.registro_nombre.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.registro_nombre.CustomButton.UseSelectable = true;
+            this.registro_nombre.CustomButton.Visible = false;
+            this.registro_nombre.Lines = new string[0];
+            this.registro_nombre.Location = new System.Drawing.Point(23, 143);
+            this.registro_nombre.MaxLength = 32767;
+            this.registro_nombre.Multiline = true;
+            this.registro_nombre.Name = "registro_nombre";
+            this.registro_nombre.PasswordChar = '\0';
+            this.registro_nombre.PromptText = "NOMBRE";
+            this.registro_nombre.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.registro_nombre.SelectedText = "";
+            this.registro_nombre.SelectionLength = 0;
+            this.registro_nombre.SelectionStart = 0;
+            this.registro_nombre.ShortcutsEnabled = true;
+            this.registro_nombre.Size = new System.Drawing.Size(328, 41);
+            this.registro_nombre.TabIndex = 0;
+            this.registro_nombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.registro_nombre.UseSelectable = true;
+            this.registro_nombre.WaterMark = "NOMBRE";
+            this.registro_nombre.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.registro_nombre.WaterMarkFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // registro_apellidos
+            // 
+            // 
+            // 
+            // 
+            this.registro_apellidos.CustomButton.Image = null;
+            this.registro_apellidos.CustomButton.Location = new System.Drawing.Point(288, 1);
+            this.registro_apellidos.CustomButton.Name = "";
+            this.registro_apellidos.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.registro_apellidos.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.registro_apellidos.CustomButton.TabIndex = 1;
+            this.registro_apellidos.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.registro_apellidos.CustomButton.UseSelectable = true;
+            this.registro_apellidos.CustomButton.Visible = false;
+            this.registro_apellidos.Lines = new string[0];
+            this.registro_apellidos.Location = new System.Drawing.Point(23, 199);
+            this.registro_apellidos.MaxLength = 32767;
+            this.registro_apellidos.Multiline = true;
+            this.registro_apellidos.Name = "registro_apellidos";
+            this.registro_apellidos.PasswordChar = '\0';
+            this.registro_apellidos.PromptText = "APELLIDOS";
+            this.registro_apellidos.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.registro_apellidos.SelectedText = "";
+            this.registro_apellidos.SelectionLength = 0;
+            this.registro_apellidos.SelectionStart = 0;
+            this.registro_apellidos.ShortcutsEnabled = true;
+            this.registro_apellidos.Size = new System.Drawing.Size(328, 41);
+            this.registro_apellidos.TabIndex = 0;
+            this.registro_apellidos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.registro_apellidos.UseSelectable = true;
+            this.registro_apellidos.WaterMark = "APELLIDOS";
+            this.registro_apellidos.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.registro_apellidos.WaterMarkFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // register_password
+            // 
+            // 
+            // 
+            // 
+            this.register_password.CustomButton.Image = null;
+            this.register_password.CustomButton.Location = new System.Drawing.Point(288, 1);
+            this.register_password.CustomButton.Name = "";
+            this.register_password.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.register_password.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.register_password.CustomButton.TabIndex = 1;
+            this.register_password.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.register_password.CustomButton.UseSelectable = true;
+            this.register_password.CustomButton.Visible = false;
+            this.register_password.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.register_password.Lines = new string[0];
+            this.register_password.Location = new System.Drawing.Point(23, 256);
+            this.register_password.MaxLength = 32767;
+            this.register_password.Name = "register_password";
+            this.register_password.PasswordChar = '●';
+            this.register_password.PromptText = "CONTRASEÑA";
+            this.register_password.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.register_password.SelectedText = "";
+            this.register_password.SelectionLength = 0;
+            this.register_password.SelectionStart = 0;
+            this.register_password.ShortcutsEnabled = true;
+            this.register_password.Size = new System.Drawing.Size(328, 41);
+            this.register_password.TabIndex = 0;
+            this.register_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.register_password.UseSelectable = true;
+            this.register_password.UseSystemPasswordChar = true;
+            this.register_password.WaterMark = "CONTRASEÑA";
+            this.register_password.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.register_password.WaterMarkFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // registro_passwordConfirn
+            // 
+            // 
+            // 
+            // 
+            this.registro_passwordConfirn.CustomButton.Image = null;
+            this.registro_passwordConfirn.CustomButton.Location = new System.Drawing.Point(288, 1);
+            this.registro_passwordConfirn.CustomButton.Name = "";
+            this.registro_passwordConfirn.CustomButton.Size = new System.Drawing.Size(39, 39);
+            this.registro_passwordConfirn.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.registro_passwordConfirn.CustomButton.TabIndex = 1;
+            this.registro_passwordConfirn.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.registro_passwordConfirn.CustomButton.UseSelectable = true;
+            this.registro_passwordConfirn.CustomButton.Visible = false;
+            this.registro_passwordConfirn.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.registro_passwordConfirn.Lines = new string[0];
+            this.registro_passwordConfirn.Location = new System.Drawing.Point(23, 312);
+            this.registro_passwordConfirn.MaxLength = 32767;
+            this.registro_passwordConfirn.Name = "registro_passwordConfirn";
+            this.registro_passwordConfirn.PasswordChar = '●';
+            this.registro_passwordConfirn.PromptText = "CONFIRMAR CONTRASEÑA";
+            this.registro_passwordConfirn.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.registro_passwordConfirn.SelectedText = "";
+            this.registro_passwordConfirn.SelectionLength = 0;
+            this.registro_passwordConfirn.SelectionStart = 0;
+            this.registro_passwordConfirn.ShortcutsEnabled = true;
+            this.registro_passwordConfirn.Size = new System.Drawing.Size(328, 41);
+            this.registro_passwordConfirn.TabIndex = 0;
+            this.registro_passwordConfirn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.registro_passwordConfirn.UseSelectable = true;
+            this.registro_passwordConfirn.UseSystemPasswordChar = true;
+            this.registro_passwordConfirn.WaterMark = "CONFIRMAR CONTRASEÑA";
+            this.registro_passwordConfirn.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.registro_passwordConfirn.WaterMarkFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // checkbox_vistaContrasela
+            // 
+            this.checkbox_vistaContrasela.AutoSize = true;
+            this.checkbox_vistaContrasela.Location = new System.Drawing.Point(23, 376);
+            this.checkbox_vistaContrasela.Name = "checkbox_vistaContrasela";
+            this.checkbox_vistaContrasela.Size = new System.Drawing.Size(102, 15);
+            this.checkbox_vistaContrasela.TabIndex = 3;
+            this.checkbox_vistaContrasela.Text = "Ver Contraseña";
+            this.checkbox_vistaContrasela.UseSelectable = true;
+            this.checkbox_vistaContrasela.CheckedChanged += new System.EventHandler(this.Register_Load);
+            this.checkbox_vistaContrasela.CheckStateChanged += new System.EventHandler(this.Register_Load);
+            this.checkbox_vistaContrasela.MouseClick += new System.Windows.Forms.MouseEventHandler(this.hacerClick);
+            // 
+            // icoUCR
+            // 
+            this.icoUCR.Image = global::SistemaContable_UCR_VIEWS.Properties.Resources.ucr_favicon;
+            this.icoUCR.Location = new System.Drawing.Point(-14, 7);
+            this.icoUCR.Name = "icoUCR";
+            this.icoUCR.Size = new System.Drawing.Size(74, 60);
+            this.icoUCR.TabIndex = 2;
+            // 
+            // registro_Atras
+            // 
+            this.registro_Atras.ActiveControl = null;
+            this.registro_Atras.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.registro_Atras.Location = new System.Drawing.Point(23, 416);
+            this.registro_Atras.Name = "registro_Atras";
+            this.registro_Atras.Size = new System.Drawing.Size(137, 108);
+            this.registro_Atras.TabIndex = 1;
+            this.registro_Atras.Text = "ATRAS";
+            this.registro_Atras.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.registro_Atras.TileImage = global::SistemaContable_UCR_VIEWS.Properties.Resources.icons8_Long_Arrow_Left_48;
+            this.registro_Atras.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.registro_Atras.UseCustomBackColor = true;
+            this.registro_Atras.UseSelectable = true;
+            this.registro_Atras.UseTileImage = true;
+            this.registro_Atras.Click += new System.EventHandler(this.registro_Atras_Click);
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.metroTile1.Location = new System.Drawing.Point(214, 416);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(137, 108);
+            this.metroTile1.TabIndex = 1;
+            this.metroTile1.Text = "REGISTRAR";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.metroTile1.TileImage = global::SistemaContable_UCR_VIEWS.Properties.Resources.icons8_Add_User_Male_48;
+            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.UseCustomBackColor = true;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.UseTileImage = true;
             // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(439, 644);
+            this.ClientSize = new System.Drawing.Size(374, 542);
+            this.Controls.Add(this.checkbox_vistaContrasela);
+            this.Controls.Add(this.icoUCR);
+            this.Controls.Add(this.registro_Atras);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.registro_passwordConfirn);
+            this.Controls.Add(this.register_password);
+            this.Controls.Add(this.registro_apellidos);
+            this.Controls.Add(this.registro_nombre);
+            this.Controls.Add(this.registro_dni);
+            this.MaximizeBox = false;
             this.Name = "Register";
-            this.Text = "Register";
+            this.Resizable = false;
+            this.Text = "Registro";
             this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
             this.Load += new System.EventHandler(this.Register_Load);
+            this.Click += new System.EventHandler(this.Register_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTextBox registro_dni;
+        private MetroFramework.Controls.MetroTextBox registro_nombre;
+        private MetroFramework.Controls.MetroTextBox registro_apellidos;
+        private MetroFramework.Controls.MetroTextBox register_password;
+        private MetroFramework.Controls.MetroTextBox registro_passwordConfirn;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile registro_Atras;
+        private System.Windows.Forms.Label icoUCR;
+        private MetroFramework.Controls.MetroCheckBox checkbox_vistaContrasela;
     }
 }
--- SistemaContable_UCR/SistemaContable_UCR_VIEWS/Register.cs
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/Register.cs
@@ -21,6 +21,38 @@ namespace SistemaContable_UCR_VIEWS
         private void Register_Load(object sender, EventArgs e)
         {
 
+            
+               
+            
+
+        }
+
+        private void registro_Atras_Click(object sender, EventArgs e)
+        {
+            Login lo = new Login();
+            lo.Visible = true;
+            this.Close();
+
+
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void hacerClick(object sender, MouseEventArgs e)
+        {
+
+            if (checkbox_vistaContrasela.Checked) {
+                register_password.UseSystemPasswordChar = false;
+                registro_passwordConfirn.UseSystemPasswordChar = false;
+            } else {
+
+                register_password.UseSystemPasswordChar = true;
+                registro_passwordConfirn.UseSystemPasswordChar = true;
+            }
+           
         }
     }
 }
