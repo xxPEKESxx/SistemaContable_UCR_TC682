0a91790e6879bd815331165ecea9c2927ebfe546
Omar PC <omarmiranda.02@gmail.com>
2017-11-11 10:14:07 -0600

    Frm_Compras added

---
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/FRM_Compras.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SistemaContable_UCR_VIEWS
+{
+    partial class FRM_Compras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.producto_Edita = new MetroFramework.Controls.MetroTile();
+            this.producto_Atras = new MetroFramework.Controls.MetroTile();
+            this.btn_agreagar_user = new MetroFramework.Controls.MetroTile();
+            this.dataGrip_listaProductos = new MetroFramework.Controls.MetroGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrip_listaProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // producto_Edita
+            // 
+            this.producto_Edita.ActiveControl = null;
+            this.producto_Edita.Location = new System.Drawing.Point(263, 156);
+            this.producto_Edita.Name = "producto_Edita";
+            this.producto_Edita.Size = new System.Drawing.Size(80, 106);
+            this.producto_Edita.TabIndex = 15;
+            this.producto_Edita.Text = "EDITAR";
+            this.producto_Edita.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.producto_Edita.TileImage = global::SistemaContable_UCR_VIEWS.Properties.Resources.appbar_clipboard_variant_edit;
+            this.producto_Edita.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.producto_Edita.UseSelectable = true;
+            this.producto_Edita.UseTileImage = true;
+            // 
+            // producto_Atras
+            // 
+            this.producto_Atras.ActiveControl = null;
+            this.producto_Atras.Location = new System.Drawing.Point(409, 156);
+            this.producto_Atras.Name = "producto_Atras";
+            this.producto_Atras.Size = new System.Drawing.Size(80, 106);
+            this.producto_Atras.TabIndex = 16;
+            this.producto_Atras.Text = "ATRAS..";
+            this.producto_Atras.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.producto_Atras.TileImage = global::SistemaContable_UCR_VIEWS.Properties.Resources.appbar_arrow_left;
+            this.producto_Atras.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.producto_Atras.UseSelectable = true;
+            this.producto_Atras.UseTileImage = true;
+            // 
+            // btn_agreagar_user
+            // 
+            this.btn_agreagar_user.ActiveControl = null;
+            this.btn_agreagar_user.Location = new System.Drawing.Point(65, 156);
+            this.btn_agreagar_user.Name = "btn_agreagar_user";
+            this.btn_agreagar_user.Size = new System.Drawing.Size(135, 106);
+            this.btn_agreagar_user.TabIndex = 17;
+            this.btn_agreagar_user.Text = "AGREGAR VENTA";
+            this.btn_agreagar_user.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_agreagar_user.TileImage = global::SistemaContable_UCR_VIEWS.Properties.Resources.appbar_thumb_up_add;
+            this.btn_agreagar_user.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_agreagar_user.UseSelectable = true;
+            this.btn_agreagar_user.UseTileImage = true;
+            // 
+            // dataGrip_listaProductos
+            // 
+            this.dataGrip_listaProductos.AllowUserToResizeRows = false;
+            this.dataGrip_listaProductos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGrip_listaProductos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGrip_listaProductos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGrip_listaProductos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrip_listaProductos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGrip_listaProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGrip_listaProductos.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGrip_listaProductos.EnableHeadersVisualStyles = false;
+            this.dataGrip_listaProductos.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dataGrip_listaProductos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGrip_listaProductos.Location = new System.Drawing.Point(65, 268);
+            this.dataGrip_listaProductos.Name = "dataGrip_listaProductos";
+            this.dataGrip_listaProductos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrip_listaProductos.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGrip_listaProductos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGrip_listaProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrip_listaProductos.Size = new System.Drawing.Size(424, 283);
+            this.dataGrip_listaProductos.TabIndex = 14;
+            // 
+            // FRM_Compras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 567);
+            this.Controls.Add(this.producto_Edita);
+            this.Controls.Add(this.producto_Atras);
+            this.Controls.Add(this.btn_agreagar_user);
+            this.Controls.Add(this.dataGrip_listaProductos);
+            this.Name = "FRM_Compras";
+            this.Text = "Compras";
+            this.Load += new System.EventHandler(this.FRM_backup_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrip_listaProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile producto_Edita;
+        private MetroFramework.Controls.MetroTile producto_Atras;
+        private MetroFramework.Controls.MetroTile btn_agreagar_user;
+        private MetroFramework.Controls.MetroGrid dataGrip_listaProductos;
+    }
+}
---
+++ SistemaContable_UCR/SistemaContable_UCR_VIEWS/FRM_Compras.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using SistemaContable_UCR_Busisness;
+using SistemaContable_UCR_Model;
+
+namespace SistemaContable_UCR_VIEWS
+{
+    public partial class FRM_Compras : MetroForm
+    {
+        public FRM_Compras()
+        {
+            InitializeComponent();
+            Transacciones pr = new Transacciones();
+
+            CoordinadorDeTransacciones cp = new CoordinadorDeTransacciones();
+            List<Transacciones> data = cp.getByType(1);
+            DataTable _table = ConvertirListaToDataTable(data);
+
+
+
+            //foreach (Productos item in data)
+            //{
+            //   DataRow dr = _table.NewRow();
+            //  dr[0] = item;
+            //  _table.Rows.Add(dr);
+            //}
+            dataGrip_listaProductos.DataSource = _table;
+        }
+
+        private void FRM_backup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public DataTable ConvertirListaToDataTable(System.Collections.IList data)
+        {
+            Transacciones pr = new Transacciones();
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(Transacciones));
+
+            DataTable table = new DataTable();
+
+            foreach (PropertyDescriptor prop in properties) table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+
+            foreach (Transacciones item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties) row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+    }
+}
