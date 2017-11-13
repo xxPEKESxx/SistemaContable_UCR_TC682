e07a97d52f3c1c6b1d1e1f933e368e2acadf7e95
Randal Vinicio Trejos Rodríguez <ranvitr@hotmail.com>
2017-11-13 09:49:26 -0600

    Agregacion de gastos

---
+++ SistemaContable_UCR/SistemaContable_UCR_Model/Gastos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaContable_UCR_Model
+{
+    public class Gastos
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public float Total { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
